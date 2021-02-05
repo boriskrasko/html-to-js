ec766f5cd6deeb59275e06aa3bb8a3887ed08873
Boris Krasko <boris.krasko@icloud.com>
2021-02-05 15:39:13 +0100

    feat: change logic

--- main.js
+++ main.js
@@ -1,41 +1,158 @@
-const inputHTML = document.querySelector('.input');
-const outputJS = document.querySelector('.output');
+const inputHtml = document.querySelector('.input');
+const outputJavaScript = document.querySelector('.output');
 const convertBtn = document.querySelector('.convert-btn');
 
-let indexOfOpeningTag;
-let indexOfClosingTag;
-let indexOfSpace;
+const signs = ['\n', '<', ' ',];
+
+let lastCharOfString;
+let stringFromHtmlCode;
+let trimmedString;
+let opennigTag;
 let tagName;
-let classNames;
-let indexOfBeginningClassName;
-let indexOfEndClassName;
-let spacesCount;
+let indexOfSpace;
+let hasClass;
+let classContent;
+let classesCount = 0;
+let className;
+let classContentBeforeTrimming;
 
-let createElementDOM = () => {
- indexOfOpeningTag = inputHTML.value.indexOf('<');
- indexOfClosingTag = inputHTML.value.indexOf('>');
- indexOfSpace = inputHTML.value.indexOf(' ');
+inputHtml.value = `<div class="container flex-box greeting">          
+ <h1 class="title"></h1>
+ <h2 class="subtitle"></h2>
+ <ul class="list items">
+  <li></li>
+  <li></li>
+ </ul>
+</div>`;
 
- if (indexOfSpace >= 0) {
-  tagName = inputHTML.value.slice(indexOfOpeningTag + 1, indexOfSpace);
- } else {
-  tagName = inputHTML.value.slice(indexOfOpeningTag + 1, indexOfClosingTag);
- }
-
- indexOfBeginningClassName = inputHTML.value.indexOf('class="');
- indexOfEndClassName = inputHTML.value.indexOf('"', indexOfBeginningClassName + 7);
- className = inputHTML.value.slice(indexOfBeginningClassName + 7,  indexOfEndClassName);
- let re = / /gi;
- let classNames = className.replace(re, "', '");
-
- if (indexOfOpeningTag >= 0) {
-  outputJS.value = `const x = document.createElement('${tagName}');\n`;
-  if(indexOfBeginningClassName > 0) outputJS.value += `x.classList.add('${classNames}');\n`;
-  outputJS.value += `root.appendChild(x);`;
-  if (inputHTML.value[indexOfClosingTag + 1] == '\n') console.log('new string');
- } else {
-  outputJS.value = `The code isn't html`;
- }
+let getLastCharOfString = (x) => {
+ lastCharOfString = x.indexOf(`\n`);
 }
 
-convertBtn.addEventListener('click', createElementDOM)
+let getStringFromHtmlCode = () => {
+  stringFromHtmlCode = inputHtml.value.slice(0, lastCharOfString);
+}
+
+let removeFirstStringFromHtml = () => {
+  inputHtml.value = inputHtml.value.replace(stringFromHtmlCode + signs[0], '');
+}
+
+let getTrimmedString = () => {
+  trimmedString = stringFromHtmlCode.trim();
+}
+
+let getIndexOfChar = (str, char) => {
+  indexOfChar = str.indexOf(char);
+}
+
+let createDOMElement = () => {
+  outputJavaScript.value += `const name = document.createElement('${tagName}');\n`;
+  trimmedString = trimmedString.replace(`${tagName}`, '');
+}
+
+let checkClass = () => {
+  hasClass = (trimmedString.indexOf('class') !== -1) ? true : false;
+}
+
+let getClassContent =  () => {
+ classContent = trimmedString.slice(trimmedString.indexOf('="') + 2, trimmedString.lastIndexOf('"'));
+}
+
+let removeExtraSpacesFromClassContent = () => {
+  classContentBeforeTrimming = classContent;
+  classContent = classContent.replace(/ +/g, ' ').trim();
+}
+
+let checkClassesCount = () => {
+  classesCount = 1;
+  getClassContent();
+  classContent = classContent.trim()
+  if (classContent.indexOf(' ') === -1) {
+    classesCount = 1;
+  } else {
+    removeExtraSpacesFromClassContent();
+    for (let i = 0; i < classContent.length; i++) {
+      if (classContent[i] == ' ') {
+        classesCount++;
+      }
+    }
+  }
+}
+
+let checkAttributes = () => {
+ hasAttr = (trimmedString.indexOf(`"`) !== -1) ? true : false;
+ console.log(hasAttr);
+}
+
+let addClassToElement = () => {
+  outputJavaScript.value += `name.classList.add('${className}');\n`;
+  trimmedString = trimmedString.replace(`class="${classContentBeforeTrimming}"`, '');
+  console.log(trimmedString);
+  checkAttributes();
+}
+
+let getClassName = () => {
+  className = classContent;
+  addClassToElement();
+}
+
+let addClassesToElement = () => {
+  className = classNames;
+  addClassToElement();
+}
+
+let getClassNames = () => {
+  re = / /gi;
+  classNames = classContent.replace(re, `', '`);
+  addClassesToElement();
+}
+
+let getAttr = () => {
+  checkClass();
+  if (hasClass) {
+    console.log('has class');
+    checkClassesCount();
+    if (classesCount === 1) {
+      getClassName();
+    } else {
+      getClassNames();
+    }
+  } else {
+    console.log('doesn`t class');
+    //
+  }
+}
+
+let getOpennigTag = () => {
+  getTrimmedString()
+  if (trimmedString[0] !== signs[1]) {
+    outputJavaScript.value =  `Your code must start with the '<'`;
+  } else if (trimmedString[1] == signs[2]) {
+    outputJavaScript.value = `There can be no space after '<'`;
+  } else {
+    getIndexOfChar(trimmedString, ' ');
+    indexOfSpace = indexOfChar;
+    if (indexOfSpace === -1) {
+      outputJavaScript.value += ` tag without attributes `;
+      if (trimmedString[trimmedString.length - 1] == '>') {
+        opennigTag = trimmedString.slice(1, trimmedString.length - 1);
+        tagName = opennigTag;
+        createDOMElement();
+      }
+    } else {
+       console.log(indexOfSpace);
+       opennigTag = trimmedString.slice(1, indexOfSpace);
+       tagName = opennigTag;
+       createDOMElement();
+       getAttr();
+    }
+  }
+}
+
+getLastCharOfString(inputHtml.value);
+getStringFromHtmlCode();
+getOpennigTag();
+// removeFirstStringFromHtml();
+
+// console.log(stringFromHtmlCode);
+// console.log(inputHtml.value);
