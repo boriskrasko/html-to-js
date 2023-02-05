0e68191699b76e1673496d6ee6519f282c3375bc
Boris Krasko <boris.krasko@live.com>
2023-02-05 15:38:40 +0100

    refactor: minor updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/* eslint-disable import/extensions */
 import './js/main.js';
-// import './sass/main.scss';
 import './css/style.css';
 
-// require.context('./images/', true, /\.(png|jpg|gif)$/);
 require.context('./icons/', true, /\.svg$/);
